15687a3b6b6e001b466086db91e73129a702fd2f
AdrianNeshad <Adrian.neshad1@gmail.com>
2025-05-31 20:31:29 +0200

    Initial Commit

---
+++ SwipeFlix/SwipeFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeFlixApp.swift
+//  SwipeFlix
+//
+//  Created by Adrian Neshad on 2025-05-31.
+//
+
+import SwiftUI
+
+@main
+struct SwipeFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
